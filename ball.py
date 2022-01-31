3cec9178481bcf215076821cfcf9f01001df998c
Thomas Rummel <93659558+rummeltf@users.noreply.github.com>
2022-01-31 16:57:46 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -8,8 +8,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.speed("slowest")
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
