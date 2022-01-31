46e29d95eef8fc4b736cb02be70a0675e61bb058
Thomas Rummel <93659558+rummeltf@users.noreply.github.com>
2022-01-31 16:58:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from paddles import Paddle
 from ball import Ball
 import time
 
-#initial setup of game screen
+# initial setup of game screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -23,10 +23,37 @@ screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
 
 # so the game instantly updates after each input
+right__player_score = 0
+left_player_score = 0
 gaming = True
 while gaming:
     time.sleep(.1)
     screen.update()
     ball.move()
+
+    # detect collision with ceiling or floor
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+    # paddle collision
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 280:
+        ball.paddle_bounce()
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -280:
+        ball.paddle_bounce()
+
+    # scoring and resetting
+    if ball.distance(right_paddle) > 50 and ball.xcor() > 300:
+        left_player_score += 1
+        time.sleep(2)
+        ball.goto(0, 0)
+        screen.update()
+        time.sleep(1)
+        ball.move()
+    elif ball.distance(left_paddle) > 50 and ball.xcor() < -300:
+        right__player_score += 1
+        time.sleep(2)
+        ball.goto(0, 0)
+        screen.update()
+        time.sleep(1)
+        ball.move()
 
 screen.exitonclick()
