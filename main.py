0de28e468059da973f926254c98faf08983c0292
Thomas Rummel <93659558+rummeltf@users.noreply.github.com>
2022-02-02 15:58:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddles import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # initial setup of game screen
@@ -14,6 +15,7 @@ screen.tracer(0)
 left_paddle = Paddle((-300, 0))
 right_paddle = Paddle((300, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 # controls
 screen.listen()
@@ -23,37 +25,50 @@ screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
 
 # so the game instantly updates after each input
-right__player_score = 0
-left_player_score = 0
+
+ball_speed_modifier = .1
 gaming = True
 while gaming:
-    time.sleep(.1)
+    time.sleep(ball_speed_modifier)
     screen.update()
     ball.move()
 
     # detect collision with ceiling or floor
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce()
+
     # paddle collision
     if ball.distance(right_paddle) < 50 and ball.xcor() > 280:
         ball.paddle_bounce()
+        ball_speed_modifier *= .95
+
     if ball.distance(left_paddle) < 50 and ball.xcor() < -280:
         ball.paddle_bounce()
+        ball_speed_modifier *= .95
 
     # scoring and resetting
-    if ball.distance(right_paddle) > 50 and ball.xcor() > 300:
-        left_player_score += 1
+    if ball.xcor() > 330:
+        scoreboard.left_point()
+        scoreboard.clear()
+        scoreboard.update_score()
+        ball_speed_modifier = .1
         time.sleep(2)
         ball.goto(0, 0)
         screen.update()
         time.sleep(1)
+        ball.paddle_bounce()
         ball.move()
-    elif ball.distance(left_paddle) > 50 and ball.xcor() < -300:
-        right__player_score += 1
+
+    elif ball.xcor() < -330:
+        scoreboard.right_point()
+        scoreboard.clear()
+        scoreboard.update_score()
+        ball_speed_modifier = .1
         time.sleep(2)
         ball.goto(0, 0)
         screen.update()
         time.sleep(1)
+        ball.paddle_bounce()
         ball.move()
 
 screen.exitonclick()
